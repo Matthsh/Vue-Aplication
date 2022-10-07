fbd3400f24841d79a0d1fe327ca40f799eb6be31
Mattheus Medina Ayala <mataus94@outlook.com>
2022-10-07 19:07:53 -0300

    going

--- front-god/src/App.vue
+++ front-god/src/App.vue
@@ -1,11 +1,22 @@
 <template>
-  <PrimeiroComponente />
+  <div>
+    <PrimeiroComponente />
+    <p>Testando dado de teste: {{ testando }}</p>
+    <LifeCycle />
+  </div>
+
 </template>
 
 <script>
 import PrimeiroComponente from './components/PrimeiroComponente.vue';
+import LifeCycle from './components/LifeCycle.vue';
 export default {
-    name: "App",
-    components: { PrimeiroComponente }
+  name: "App",
+  components: { PrimeiroComponente, LifeCycle },
+  data() {
+    return {
+      testando: "dado de teste"
+    }
+  }
 };
 </script>
---
+++ front-god/src/components/LifeCycle.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>Meu nome é: {{ name }}</h1>    
+</template>
+
+<script>
+import { createDOMCompilerError } from '@vue/compiler-dom';
+
+    export default {
+        name: 'LifeCycle',
+        data(){
+            return{
+                name: "Ainda não sei"
+            }
+        },
+        created(){
+            setTimeout(() => {
+                this.name = "Mattheus"
+            }, 1000)
+        },
+        mounted(){
+            setTimeout(() => {
+                this.name = "Pedro"
+            }, 2000)
+        }
+    }
+</script>
