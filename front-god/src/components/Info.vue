66f31c1918f724adfc41b5391bc18fdb51be36fa
Mattheus Medina Ayala <mataus94@outlook.com>
2022-10-17 17:10:43 -0300

    ✅ Class 10, Dinamic Attributes

--- front-god/src/components/Info.vue
+++ front-god/src/components/Info.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <Picture/>
         <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
         <p v-else>Estou em busca de novas oportunidades!</p>
         <p>Utilizo as seguintes tecnologias:</p>
@@ -9,17 +10,21 @@
             <li>Python</li>
         </ul>
         <p v-show="mostrar_email">Mande um email para: {{email}}</p>
+        <p>Para acessar o meu portifólio <a v-bind:href="portifolio">basta clicar aqui</a></p>
     </div>
 </template>
 <script>
+import Picture from './Picture.vue';
     export default {
-        name: "Info",
-        data(){
-            return {
-                esta_trabalhando: false,
-                mostrar_email: false,
-                email: "MattheusMedina@outlook.com"
-            }
-        }
-    }
+    name: "Info",
+    data() {
+        return {
+            esta_trabalhando: false,
+            mostrar_email: false,
+            email: "MattheusMedina@outlook.com",
+            portifolio: "https://google.com"
+        };
+    },
+    components: { Picture }
+}
 </script>
