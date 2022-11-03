5fbaed2c59228698b9dacae550ed23daab7d61d7
Mattheus Medina Ayala <mataus94@outlook.com>
2022-11-03 11:35:31 -0300

    🚧 Eventos (@submit e @click)

--- front-god/src/App.vue
+++ front-god/src/App.vue
@@ -14,7 +14,7 @@
 import PrimeiroComponente from './components/PrimeiroComponente.vue';
 import Pessoa from './components/Pessoa.vue';
 import LifeCycle from './components/LifeCycle.vue';
-import Form from './components/Form.vue';
+import Form from './components/form/Form.vue';
 import Header from './components/Header.vue'
 export default {
   name: "App",
---
+++ front-god/src/components/form/Form.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h2>Meu formulário:</h2>
+        <form action="" @submit="enviarFormulario($event)">
+            <div>
+                <input type="text" v-model="name" />
+            </div>
+            <div>
+                <input type="text" v-model="email" />
+            </div>
+            <div>
+                <Submit />
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+import InputText from './InputText.vue';
+import Submit from './Submit.vue';
+export default {
+    name: "Form",
+    components: { InputText, Submit },
+    methods: {
+        enviarFormulario(e) {
+            e.preventDefault()
+
+            const name = this.name;
+            const email = this.email;
+
+            console.log("Formulário enviado")
+
+            console.log("O nome é: " + name);
+            console.log("O email é: " + email);
+        }
+    },
+    data() {
+        return {
+            name: "Mattheus",
+            email: "MattheusMedina@outlook"
+        }
+    }
+}
+</script>
